377d5da3d0b0ced3091af688ebedfe64f9fa12aa
Jeff Hall <au8558@wayne.edu>
2019-03-31 19:58:31 -0400

    Update Ticket.py
    New & improved Ticket class for Python

--- PythonMYSQLConnectorTest2/Ticket.py
+++ PythonMYSQLConnectorTest2/Ticket.py
@@ -1,30 +1,47 @@
 class Ticket:
-    Ticket_ID = 0
-    Starting_Point = ''
-    Destination = ''
-    Departure_Time = 0
-    Arrival_Time = 0
-    
-    def Ticket(self, Ticket_ID, Starting_point, Destination, Departure_Time, Arrival_Time):
-        self.Ticket_ID = Ticket_ID
+
+    @property
+    def __init__(self, Starting_point, Destination, First_name, Last_name, Seat, 
+               Departure_Time, Arrival_Time, Departure_date, Arrival_date, Ticket_ID):
         self.Starting_Point = Starting_point
         self.Destintion = Destination
+        self.First_name = First_name
+        self.Last_name = Last_name
+        self.Seat = Seat
         self.Departure_Time=  Departure_Time
         self.Arrival_Time = Arrival_Time
+        self.Departure_date = Departure_date
+        self.Arrival_date = Arrival_date
+        self.Ticket_ID = Ticket_ID
         
-    def set_Ticket_ID(self, ID):
-        self.Ticket_ID = ID
-        
-    def set_Starting_Point(self, location):
-        self.Starting_Point = location
+    def set_Starting_point(self, location):
+        self.Starting_point = location
         
     def set_Destination(self, location):
         self.Destination = location
+      
+    def set_First_name(self, name):
+        self.First_name = name
         
-    def set_Departure_Time(self, time):
-        self.Departure_Time = time
+    def set_Last_name(self, name):
+        self.Last_name = name
         
-    def set_Arrival_Time(self, time):
+    def set_Seat(self, seat):
+        self.Seat = seat
+        
+    def set_Departure_time(self, time):
+        self.Departure_time = time
+        
+    def set_Arrival_time(self, time):
         self.Arrival_Time = time
+        
+    def set_Departure_date(self, date):
+        self.Departure_date = date
+        
+    def set_Arrival_date(self, date):
+        self.Arrival_date = date
+        
+    def set_Ticket_ID(self, ID):
+        self.Ticket_ID = ID
         
     
