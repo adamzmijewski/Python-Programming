fbab8d23f25afafa671f651ae69164386f2de0a0
Jeff Hall <au8558@wayne.edu>
2019-03-31 20:53:22 -0400

    Update Main.py
    Updated Main file to include functions for printing user menu and prompting for input
    NOTE:  Menu has 3 options, only option 1 works now - retrieval of ticket info.

--- PythonMYSQLConnectorTest2/Main.py
+++ PythonMYSQLConnectorTest2/Main.py
@@ -3,9 +3,43 @@ from mysql.connector.errors import Error
 import random
 import Ticket
 
+# Menu function
+def Menu(cursor):
+    printMenu()
+        
+    #input
+    prompt = input('Enter choice = ')
+        
+    while(prompt > '0' and prompt < '4'):
+        
+        if(prompt == '1'):   
+        
+            #view the ticket
+            try:
+                #cursor = conn.cursor()
+                ticket_code =  input(str('Input Ticket No.: '))
+                cursor.execute('''SELECT * FROM Ticket where Ticket_ID = '''+ticket_code)
+                for row in cursor:
+                    print(row)
+                    
+            except Error:
+                print('Failed to retrieve values')
+                
+            finally:
+                printMenu()
+                
+        prompt = input('Enter choice = ')
+                
+# Menu display
+def printMenu():
+    print('Full Monty Transit\n')
+    print('  1)View Ticket information')
+    print('  2)Reserve Ticket')
+    print('  3)Add route\n')
+
 # RANDOM TICKET NUMBER GENERATOR
 ticket_ID = str(random.randint(111111, 999999))
-print(ticket_ID)
+print('Random Ticket ID generated: %s\n' % (ticket_ID))
 
 # CONNECTION BLOCK
 try:
@@ -23,8 +57,9 @@ except ConnectionError:
     
     # INSERTION BLOCK
 try:
-    mycursor = conn.cursor()
-    mycursor.execute('''
+    cursor = conn.cursor()
+    Menu(cursor)
+    cursor.execute('''
 
                 INSERT INTO Ticket (Starting_point, Destination, First_name, Last_name, Seat, Departure_time, Arrival_time, Departure_date, Arrival_date, Ticket_ID)
                 VALUES
@@ -37,13 +72,3 @@ try:
     
 except Error:
     print("Failed to insert ticket")
-   
-# RETRIEVAL/PRINT BLOCK    
-try:
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM Ticket')
-    for row in cursor:
-        print(row)
-    
-except Error:
-    print('Failed to retrieve values')
