0d4bfe178bd95ebed3f44fc2e579ef03469a39f9
Jeff Hall <au8558@wayne.edu>
2019-04-02 17:58:38 -0400

    Update Ticket.py
    Added 'get' methods for class attributes.

--- PythonMYSQLConnectorTest2/Ticket.py
+++ PythonMYSQLConnectorTest2/Ticket.py
@@ -13,35 +13,84 @@ class Ticket:
         self.Departure_date = Departure_date
         self.Arrival_date = Arrival_date
         self.Ticket_ID = Ticket_ID
-        
+    
+    # Set/Get for Starting_point
     def set_Starting_point(self, location):
         self.Starting_point = location
         
+    def get_Starting_point(self):
+        return self.Starting_point
+        
+        
+    # Set/Get for Destination
     def set_Destination(self, location):
         self.Destination = location
+        
+    def get_Destination(self):
+        return self.Destination
       
+      
+    # Set/Get for First_name
     def set_First_name(self, name):
         self.First_name = name
         
+    def get_First_name(self):
+        return self.First_name
+    
+    
+    # Set/Get for Last_name
     def set_Last_name(self, name):
         self.Last_name = name
         
+    def get_Last_name(self):
+        return self.Last_name
+    
+    
+    # Set/Get for Seat  
     def set_Seat(self, seat):
         self.Seat = seat
         
+    def get_Seat(self):
+        return self.Seat
+    
+    
+    # Set/Get for Departure_time
     def set_Departure_time(self, time):
         self.Departure_time = time
         
+    def get_Departure_time(self):
+        return self.Departure_time
+        
+        
+    # Set/Get for Arrival_time
     def set_Arrival_time(self, time):
         self.Arrival_Time = time
         
+    def get_Arrival_time(self):
+        return self.Arrival_time
+        
+        
+    # Set/Get for Departure_date
     def set_Departure_date(self, date):
         self.Departure_date = date
         
+    def get_Departure_date(self):
+        return self.Departure_date
+        
+        
+    # Set/Get for Arrival_date
     def set_Arrival_date(self, date):
         self.Arrival_date = date
         
+    def get_Arrival_date(self):
+        return self.Arrival_date
+        
+        
+    # Set/Get for Ticket_ID
     def set_Ticket_ID(self, ID):
         self.Ticket_ID = ID
+        
+    def get_Ticket_ID(self):
+        return self.Ticket_ID
         
     
