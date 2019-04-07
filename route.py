8b0f8ecf68d7448234e57a99bdf8889527e6e975
adamzmijewski <38869013+adamzmijewski@users.noreply.github.com>
2019-04-06 22:51:50 -0400

    route.py

---
+++ route.py
@@ -0,0 +1,23 @@
+'''
+Created on Apr 2, 2019
+
+@author: adamzmijewski
+'''
+
+class Route(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, id,routeNumber,passengers,startpoint,endpoint,time,duration):
+        self.id = id
+        self.routeNumber=routeNumber
+        self.passengers=passengers
+        self.startpoint=startpoint
+        self.endpoint=endpoint
+        self.time=time
+        self.duration=duration
+        
+        
+        
